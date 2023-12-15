c46e02d0a5fd0a088774aa4e53cb3f9c5200072e
Ahmed Mahmud <am5737@columbia.edu>
2023-12-15 11:26:04 -0500

    Update SignInWithGoogle.jsx

--- src/components/SignInWithGoogle.jsx
+++ src/components/SignInWithGoogle.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import { auth } from './firebaseConfig';
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 
-
-export function SignInWithGoogle() {
+const SignInWithGoogle = () => {
   const signInWithGoogle = () => {
     const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider).then((result) => {
-      // User signed in
-    }).catch((error) => {
-      console.error("Error signing in with Google:", error);
-    });
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // User signed in
+      })
+      .catch((error) => {
+        console.error("Error signing in with Google:", error);
+      });
   };
 
   return (
     <button onClick={signInWithGoogle}>Sign in with Google</button>
   );
-}
+};
+
+export default SignInWithGoogle;
